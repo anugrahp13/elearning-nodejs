ef3ed84cd640d3e5428eb5c104202485974be5e4
Anugrah Prastyo <anugrahpraystyo13@gmail.com>
2024-06-19 20:35:02 +0700

    build - create router bab, subbab and material

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { listKelas } = require("../controllers/kelas.controller");
 const { listMataPelajaran } = require("../controllers/matapelajaran.contoller");
+const { listBab } = require("../controllers/bab.controller");
+const { listSubBab } = require("../controllers/subbab.controller");
+const { listMaterial } = require("../controllers/material.controller");
 
 //get data
 router.get("/kelas", listKelas);
-router.get("/mata-pelajaran", listMataPelajaran);
+router.get("/matapelajaran", listMataPelajaran);
+router.get("/bab", listBab);
+router.get("/subbab", listSubBab);
+router.get("/material", listMaterial);
 
 
 module.exports = {router}
